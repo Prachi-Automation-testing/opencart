8971d27ddadb6693c72d5ce3739dfa121a26db5d
Prachi <thapaprachi07@gmail.com>
2024-03-29 17:29:06 +0530

    this is my second commit

--- src/test/java/TestBase/baseclass.java
+++ src/test/java/TestBase/baseclass.java
@@ -24,7 +24,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
-public class baseclass 
+public class baseclass  ////   https://github.com/Prachi-Automation-testing/opencart.git
 
 {
 	public WebDriver driver;      // you may static as well from public
